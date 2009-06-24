8db827bb2d669b8598e90d8b90e8e2dc0dbadf76
nathan <nathan@ubuntu904desktop.localdomain>
2009-06-24 14:24:28 -0400

    modified hudson package... again

--- packages/activemq.rb
+++ packages/activemq.rb
@@ -1,25 +1,28 @@
 require 'package'
 require 'packages/general'
+require 'open-uri'
+require 'fileutils'
+include FileUtils
 
 package(:activemq) {
   depends_on :java, :svn
 
   install {
     system("wget http://mirror-fpt-telecom.fpt.net/apache/activemq/apache-activemq/5.2.0/apache-activemq-5.2.0-bin.tar.gz")
-    system("tar xf apache-activemq-5.2.0-bin.tar.gz")
-    system("mv -v apache-activemq-5.2.0 /opt/apache-activemq-5.2.0")
-    system("ln -sv /opt/apache-activemq-5.2.0 /opt/active-mq")
-    system("ln -sv /opt/apache-activemq-5.2.0/bin/linux-x86-32 /opt/apache-activemq-5.2.0/bin/linux")
-    system("cp -v support/activemq /etc/init.d/")
-    system("chmod a+x /etc/init.d/activemq")
-    system("update-rc.d activemq defaults")
+    shell_out("tar xf apache-activemq-5.2.0-bin.tar.gz")
+    mv 'apache-activemq-5.2.0', '/opt/apache-activemq-5.2.0'
+    ln_s '/opt/apache-activemq-5.2.0', '/opt/active-mq'
+    ln_s '/opt/apache-activemq-5.2.0/bin/linux-x86-32', '/opt/apache-activemq-5.2.0/bin/linux'
+    cp 'support/activemq', '/etc/init.d/'
+    chmod 0005, '/etc/init.d/activemq'
+    shell_out("update-rc.d activemq defaults")
   }
   
   remove {
-    system("rm -rf /opt/active-mq")
-    system("rm -rf /opt/apache-activemq-5.2.0")
-    system("update-rc.d -f activemq remove")
-    systme("rm /etc/init.d/activemq")
+    rm_rf '/opt/active-mq'
+    rm_rf '/opt/apache-activemq-5.2.0'
+    shell_out("update-rc.d -f activemq remove")
+    rm '/etc/init.d/activemq'
   }
 
   installed? {
--- packages/hudson.rb
+++ packages/hudson.rb
@@ -4,18 +4,19 @@ require 'packages/general'
 package(:hudson) {
   depends_on :java
   install {
-    system("wget https://hudson.dev.java.net/files/documents/2402/136743/hudson.war")
+    system("wget http://hudson-ci.org/latest/hudson.war")
     system("mkdir -v /opt/hudson")
     system("mv -v hudson.war /opt/hudson/")
     system("cp -v support/run-hudson /opt/hudson/")
     system("cp -v support/hudson /etc/init.d/")
     system("update-rc.d hudson defaults")
+    system("chmod a+rw /opt/hudson/run-hudson")
+    system("chmod a+rw /etc/init.d/hudson")
   }
   remove {
-    system("update-rc.d hudson remove")
+    system("update-rc.d -f hudson remove")
     system("rm -rfv /opt/hudson/")
     system("rm -v /etc/init.d/hudson")
-    system("rm -v /etc/init.d/run-hudson")
   }
   installed? {
     File.exists? "/opt/hudson/hudson.war"
