d96733bd289f519515a80ee0ab58671c8253cb0d
nathan <nathan@ubuntu904desktop.localdomain>
2009-06-26 16:59:17 -0400

    fixed curl dependency problem

--- packages/build_server.rb
+++ packages/build_server.rb
@@ -5,9 +5,9 @@ require 'packages/tdsurface'
 require 'packages/auto_installer'
 
 meta_package(:build_server) {
-  depends_on :curl
   consists_of :hudson, :auto_installer, :tdsurface
   after_install {
+    shell_out("aptitude -y install curl")
     shell_out("curl -d@#@support/hudson/config.xml -H \"Content-Type: text/xml\" \"http://localhost:8080/createItem?name=autoinstaller\"")
   }
 }    
