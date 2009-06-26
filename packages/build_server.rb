363b096ea2527436f14efbb1c61453ad7c3648ed
nathan <nathan@ubuntu904desktop.localdomain>
2009-06-26 17:29:16 -0400

    modified build_server to retry

--- packages/build_server.rb
+++ packages/build_server.rb
@@ -5,9 +5,20 @@ require 'packages/tdsurface'
 require 'packages/auto_installer'
 
 meta_package(:build_server) {
-  consists_of :hudson, :auto_installer, :tdsurface
+  consists_of :hudson, :tdsurface
   after_install {
     shell_out("aptitude -y install curl")
-    shell_out("curl -d@#@support/hudson/config.xml -H \"Content-Type: text/xml\" \"http://localhost:8080/createItem?name=autoinstaller\"")
+    retry_count = 10
+    tries = 0
+    finished = false
+    while (tries < retry_count) and not finished
+      begin
+        shell_out("curl -d@#@support/hudson/config.xml -H \"Content-Type: text/xml\" \"http://localhost:8080/createItem?name=autoinstaller\"")
+      rescue
+        tries += 1
+        finished = false
+      end
+      finished = true
+    end
   }
 }    
