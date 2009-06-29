9a0494ea0cf6d3e36e1cabe39b639f89a4c1a5fc
nathan <nathan@ubuntu904desktop.localdomain>
2009-06-29 11:22:28 -0400

    fixed bug in naming of tdsurface job

--- packages/build_server.rb
+++ packages/build_server.rb
@@ -12,7 +12,7 @@ package(:build_server) {
     client.post('http://localhost:8080/createItem?name=autoinstaller', 
                 File.open("#@support/hudson/auto-installer-config.xml").read,
                 {'Content-Type' => 'text/xml'})
-    client.post('http://localhost:8080/createItem?name=autoinstaller',
+    client.post('http://localhost:8080/createItem?name=tdsurface',
                 File.open("#@support/hudson/tdsurface-config.xml").read,
                 {'Content-Type' => 'text/xml'})
   }
