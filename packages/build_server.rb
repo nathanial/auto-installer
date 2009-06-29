263db6b9f05ba3150c31089060f3b40c55d33f6c
nathan <nathan@ubuntu904desktop.localdomain>
2009-06-29 11:15:50 -0400

    modified build_server to use httpclient

--- packages/build_server.rb
+++ packages/build_server.rb
@@ -8,17 +8,9 @@ package(:build_server) {
   depends_on :hudson, :tdsurface, :curl
   
   install {
-    retry_count = 10
-    tries = 0
-    finished = false
-    while (tries < retry_count) and not finished
-      begin
-        shell_out("curl -d@#@support/hudson/config.xml -H \"Content-Type: text/xml\" \"http://localhost:8080/createItem?name=autoinstaller\"")
-      rescue
-        tries += 1
-        finished = false
-      end
-      finished = true
-    end
+    client = HTTPClient.new
+    client.post('http://localhost:8080/createItem?name=autoinstaller', 
+                File.open("#@support/hudson/config.xml").read,
+                {'Content-Type' => 'text/xml'})
   }
 }    
