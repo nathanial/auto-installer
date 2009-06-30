424b1ca7140a29ed1e1b017cfc481fb70a7551af
nathan <nathan@ubuntu904desktop.localdomain>
2009-06-30 16:52:33 -0400

    localized tdsurface

--- packages/tdsurface.rb
+++ packages/tdsurface.rb
@@ -10,7 +10,10 @@ package(:tdsurface) {
 
   python_site_packages = `python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"`.chomp
 
+  password = SETTINGS[:tdsurface][:password]
+
   install {
+    process_support_files
     mkdir_p(['/var/django-projects', '/var/matplotlib', '/var/log/tdsurface'])
     shell_out("git clone git@github.com:teledrill/tdsurface.git /var/django-projects/tdsurface")
     cp "#@support/tdsurface/django_local_settings.py", "/var/django-projects/tdsurface/settings_local.py"
@@ -18,9 +21,9 @@ package(:tdsurface) {
     cp_r "#{python_site_packages}/django/contrib/admin/media", "/var/www/media"
     cp_r "/var/django-projects/tdsurface/media","/var/www/"
     shell_out("usermod -a -G dialout www-data")
-    shell_out("""mysql --user=root --password=scimitar1 -e \"
+    shell_out("""mysql --user=root --password=#{password} -e \"
 CREATE DATABASE tdsurface;
-CREATE USER 'tdsurface'@'localhost' IDENTIFIED BY 'scimitar1';
+CREATE USER 'tdsurface'@'localhost' IDENTIFIED BY '#{password}';
 GRANT ALL PRIVILEGES ON *.* TO 'tdsurface'@'localhost';\"
 """)
     shell_out("expect #@support/tdsurface/expect_script.tcl")
@@ -37,7 +40,7 @@ GRANT ALL PRIVILEGES ON *.* TO 'tdsurface'@'localhost';\"
     rm_rf '/var/log/tdsurface'
     rm_rf '/var/www/media'
     system("""
-    mysql --user=root --password=scimitar1 -e \"
+    mysql --user=root --password=#{password} -e \"
        DROP DATABASE tdsurface;
        DROP USER 'tdsurface'@'localhost';\"
 """)
