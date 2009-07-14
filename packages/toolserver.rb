b0631c9eb2ffbb9090d013d908f0d661fbff94bd
nathan <nathan@ubuntu904desktop.localdomain>
2009-07-14 10:45:12 -0500

    added toolserver package

---
+++ packages/toolserver.rb
@@ -0,0 +1,27 @@
+require 'fileutils'
+include FileUtils
+include Logging
+
+class ToolServer < Package
+  depends_on :python, :git
+
+  def install 
+    mkdir_p "/var/django-projects/"
+    shell_out("git clone git@github.com:teledrill/tdtoold.git /var/django-projects/tdtoold")
+    shell_out("cd /var/django-projects/tdtoold && python setup.py install")
+    cp "/var/django-projects/tdtoold/init.d/tdtoold", "/etc/init.d/"
+    shell_out("update-rc.d tdtoold defaults")
+    shell_out("service tdtoold start")
+  end
+  
+  def remove 
+    shell_out_force("service tdtoold stop")
+    shell_out_force("update-rc.d -f tdtoold remove")
+    rm_rf "/var/django-projects/tdtoold"
+  end
+
+  def installed?
+    File.exists? "/var/django-projects/tdtoold"
+  end
+end
+Packages.register(:toolserver, ToolServer.new(:toolserver))
