2aa8433d33d22286c5153a5235860be675da23ec
nathan <nathan@ubuntu904desktop.localdomain>
2009-07-31 15:27:53 -0400

    added compojure package

---
+++ packages/compojure.rb
@@ -0,0 +1,23 @@
+require 'fileutils'
+include FileUtils
+
+class Compojure < Package
+  name :compojure
+  depends_on :clojure
+  repository :git, "git://github.com/weavejester/compojure.git"
+  
+  def install 
+    shell_out("ant deps")
+    shell_out("ant")
+    cp "#@project_directory/compojure.jar", "/usr/local/lib/clojure/"
+    cp "#@support/compojure/compojure", "/usr/local/bin/"
+    cp_r "#@project_directory/deps", "/usr/local/lib/clojure/deps"
+    chmod 0555, "/usr/local/bin/compojure"
+  end
+
+  def remove
+    rm_f "/usr/local/lib/clojure/compojure.jar"
+    rm_f "/usr/local/bin/compojure"
+    rm_rf "/usr/local/lib/clojure/deps"
+  end
+end
